7e8158cebc34a2c810a28af6537135f0b8703a7c
phxtho <phathopukwana@gmail.com>
2023-04-20 19:03:11 +0200

    adjust image planes dimensions

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import "./main.css";
 // @ts-ignore
 import dicomFile from "./assets/case1_008.dcm";
-import cornerstone, { Image } from "cornerstone-core";
+import cornerstone from "cornerstone-core";
 // @ts-ignore
 import cornerstoneWADOImageLoader from "cornerstone-wado-image-loader";
 import dicomParser from "dicom-parser";
@@ -21,16 +21,18 @@ const { imageFrame } = await cornerstone.loadAndCacheImage(
 );
 
 const texture = createTexture(imageFrame);
+const aspect = imageFrame.columns / imageFrame.rows;
+const width = 7;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Canvas>
       <color attach="background" args={["white"]} />
-      <camera position={[0, 0, -20]} />
+      <camera position={[0, 0, -30]} />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <mesh position={[0, 0, 0]}>
-        <planeGeometry args={[imageFrame.columns, imageFrame.rows, 1, 1]} />
+        <planeGeometry args={[width, width * aspect, 1, 1]} />
         <meshBasicMaterial map={texture} side={THREE.DoubleSide} />
       </mesh>
       <OrbitControls />
