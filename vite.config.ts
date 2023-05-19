949a1e6732830770a5318d59d4ee889b25ce6cee
phxtho <phathopukwana@gmail.com>
2023-05-19 18:02:43 +0200

    remove leading slash in vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "//medical-imaging-3js/", // to use gh-pages
+  base: "/medical-imaging-3js/", // to use gh-pages
   assetsInclude: ["**/*.dcm"],
 });
